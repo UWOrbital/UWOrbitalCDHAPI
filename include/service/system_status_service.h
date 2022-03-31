51bd8e19df9cdb7adc29eac97d296c877aef9b06
Kevin Le <k26le@uwaterloo.ca>
2022-03-30 20:38:58 -0600

    Format docs, add params and port enum

--- include/service/system_status_service.h
+++ include/service/system_status_service.h
@@ -1,38 +1,39 @@
-/*
+/**
 * Service responsible for reboot, emergency mode, and resolving system errors
 */
 
 
-/*
-* System health data encompassing all submodules 
+/**
+  System health data encompassing all submodules 
 */
 typedef struct {
 	// TODO: Add important system health parameters
 } sys_health_t;
 
-/*
-* Reboots the system.
-* Should only be used if necessary.
-* @return 0 if successful, else error code
+/**
+  Reboots the system.
+  Should only be used if necessary.
+  @return 1 if success, 0 on fail
 */
 int sys_reboot(void);
 
-/*
-* Changes state of satellite to emergency mode in the case of an error.
-* Should be used purely as a state changer
-* @return 0 if successful state chance, else error code
+/**
+  Changes state of satellite to emergency mode in the case of an error.
+  Should be used purely as a state changer
+  @return 1 if successful state change, else 0 on fail
 */
 int sys_emergency_state();
 
-/*
-* Chooses how to handle the error, should use system reboot as last resort.
-* @param TODO: Add parameters (error causing error handler etc.)
-* @return handling function success or error code
+/**
+  Chooses how to handle the error, should use system reboot as last resort.
+  @param TODO: Add parameters (error causing error handler etc.)
+  @param sys_health current system health data 
+  @return handling function
 */
-int sys_error_handler();
+int sys_error_handler(sys_health_t *sys_health);
 
-/*
-*  Error handling functions below
+/**
+  Error handling functions below
 */
 
 // TODO: Implement error handling functions as needed, will depend on system health params
--- include/service/telemetry_collection_service.h
+++ include/service/telemetry_collection_service.h
@@ -1,37 +1,58 @@
-/*
+/**
 * Service responsible for collecting and sending telemetry to comms.
 * Ensures data transmitted at the right time.
 */
 
-/*
-* Sends a request to submodule, collects telemetry and store in a buffer.
-* @param TODO: Add parameters (port for connection, buffer etc.)
-* @return 0 if successful, else error code
+/**
+ * Maps each subsystem to a port
+ */
+enum subsystem_port_t {
+    // TODO: Change numbers to be real ports which are TBD
+    CDH = 0,
+    COMMS = 1,
+    ADCS = 2
+};
+
+// TODO: Send and collect telemetry should be similar to
+// existing csp functions. Should use existing functions
+// when possible
+
+// TODO: Figure out if this is necessary or if built in
+// get command in sfsf will suffice
+/**
+  Sends a request to submodule, collects telemetry and store in a buffer.
+  @param TODO: Add parameters (port for connection, buffer etc.)
+  @return 0 if successful, else error code
 */
 int collect_telemetry();
 
-/*
-* Sends data to a submodule, store response in a buffer.
-* @param TODO: Add parameters (port for connection, buffer etc.)
-* @return 0 if successful, else error code
+/**
+  Sends data to a submodule, store response in a buffer.
+  @param TODO: Add parameters (port for connection, buffer etc.)
+  @return 0 if successful, else error code
 */
 int send_telemetry();
 
 
-/*
-* We may need to write a conversion function for every struct
+/**
+  We may need to write a conversion function for every struct
 */
 
-/*
-* Converts the struct to a character buffer
-* @param TODO: Add parameters (struct, buffer, etc.)
-* @return 0 if successful, else error code
+/**
+  Converts the struct to a character buffer
+  @param struct TODO: needs to be a specific struct
+  @param inbuf user provided buffer for storing converted struct
+  @param inlen length of expected buffer after conversion
+  @return 1 or buffer size on success, 0 on failure
 */
-int struct_to_char_buff();
+int struct_to_char_buff(void * inbuf, int inlen);
 
-/*
-* Converts the buffer (should be in the form of a char[]) to a struct
-* @param TODO: Add parameters (struct, buffer, etc.)
-* @return 0 if successful, else error code
+/**
+  Converts the buffer (should be in the form of a char[]) to a struct
+  @param TODO: Add parameters (struct, buffer, etc.)
+  @param struct TODO: needs to be a specific struct
+  @param outbuf outgoing data to be converted to struct
+  @param outlen length of outgoing data
+  @return 1 on success, 0 on failure
 */
-int char_buff_to_struct();
+int char_buff_to_struct(void * outbuf, int outlen);
