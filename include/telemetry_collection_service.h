da8ff611c17461ad0bd6e751758e97a0fde14992
Kevin Le <k26le@uwaterloo.ca>
2022-04-12 21:26:50 -0600

    add types and func bodies for tele service

--- include/telemetry_collection_service.h
+++ include/telemetry_collection_service.h
@@ -15,10 +15,40 @@
  */
 typedef enum {
     // TODO: Change numbers to be real ports which are TBD
-    CDH = 0,
-    COMMS = 1,
-    ADCS = 2
+    CDH_PORT = 0,
+    COMMS_PORT = 1,
+    ADCS_PORT = 2
 } subsystem_port_t;
+
+/**
+ * @enum 
+ * @brief Maps data structure of team to placeholder int
+ */
+typedef enum {
+    // TODO: modify when we figure out which subsystems need their own structs
+    CDH_PARAM = 0,
+    COMMS_PARAM = 1,
+    EPS_PARAM = 2
+} subsystem_parameters_t;
+
+/**
+ * @enum subsystem_address_t
+ * @brief Maps each subsystem to an address
+ */
+typedef enum {
+    // TODO: Change numbers to be real addresses which are TBD
+    CDH_ADDRESS = 0,
+    COMMS_ADDRESS = 1,
+    ADCS_ADDRESS = 2
+} subsystem_address_t;
+
+/**
+ * @brief store EPS subsystem parameters
+ */
+typedef struct {
+	  // TODO
+    int sys_temp;
+} eps_sys_t;
 
 // TODO: Send and collect telemetry should be similar to
 // existing csp functions. Should use existing functions
@@ -28,10 +58,17 @@ typedef enum {
 // get command in sfsf will suffice
 /**
   Sends a request to submodule, collects telemetry and store in a buffer. Usually a get command.
-  @param TODO: Add parameters (port for connection, buffer etc.)
-  @return 0 if successful, TODO: define what error codes and typedef for them
+  @param[in] prio priority, see #csp_prio_t
+  @param[in] dest destination address
+  @param[in] port destination based on submodule
+  @param[in] timeout timeout in mS to wait for a reply
+  @param[in] aux_buffer outgoing data (request) excluding command, should be 254 max
+  @param[in] outlen length of data in \a outbuf (request)
+  @param[out] inbuf user provided buffer for receiving data (reply)
+  @param[in] inlen length of expected reply, -1 for unknown size (inbuf MUST be large enough), 0 for no reply.
+  @return 1 or reply size on success, 0 on failure (error, incoming length does not match, timeout)
 */
-int collect_telemetry();
+int collect_telemetry(uint8_t prio, subsystem_address_t dest, subsystem_port_t port, uint32_t timeout, void * aux_buffer, int outlen, void * inbuf, int inlen);
 
 /**
   Sends data to a submodule, store response in a buffer. Parameters almost identical to csp_transaction. Usually a set command.
@@ -39,14 +76,14 @@ int collect_telemetry();
   @param[in] dest destination address
   @param[in] port destination based on submodule
   @param[in] timeout timeout in mS to wait for a reply
-  @param[in] outbuf outgoing data (request)
+  @param[in] aux_buffer outgoing data (request) excluding command, should be 254 max
   @param[in] outlen length of data in \a outbuf (request)
   @param[out] inbuf user provided buffer for receiving data (reply)
   @param[in] inlen length of expected reply, -1 for unknown size (inbuf MUST be large enough), 0 for no reply.
   @return 1 or reply size on success, 0 on failure (error, incoming length does not match, timeout)
 */
-int send_telemetry(uint8_t prio, uint8_t dest, subsystem_port_t port, uint32_t timeout, void * outbuf, int outlen, void * inbuf, int inlen);
-
+int send_telemetry(uint8_t prio, subsystem_address_t dest, subsystem_port_t port, uint32_t timeout, void * aux_buffer, int outlen, void * inbuf, int inlen);
+ 
 
 /**
   We may need to write a conversion function for every struct
@@ -57,9 +94,10 @@ int send_telemetry(uint8_t prio, uint8_t dest, subsystem_port_t port, uint32_t t
   @param struct TODO: needs to be a specific struct
   @param inbuf user provided buffer for storing converted struct
   @param inlen length of expected buffer after conversion
+  @param subsystem_param which type of struct we are converting
   @return 1 or buffer size on success, 0 on failure
 */
-int struct_to_char_buff(void * inbuf, uint32_t inlen);
+int struct_to_char_buff(void * inbuf, uint32_t inlen, subsystem_parameters_t subsystem_param);
 
 /**
   Converts the buffer (should be in the form of a char[]) to a struct
@@ -67,8 +105,9 @@ int struct_to_char_buff(void * inbuf, uint32_t inlen);
   @param struct TODO: needs to be a specific struct
   @param outbuf outgoing data to be converted to struct
   @param outlen length of outgoing data
+  @param subsystem_param which type of struct we are converting
   @return 1 on success, 0 on failure
 */
-int char_buff_to_struct(void * outbuf, uint32_t outlen);
+int char_buff_to_struct(void * outbuf, uint32_t outlen, subsystem_parameters_t subsystem_param);
 
 #endif
--- src/telemetry_collection_service.c
+++ src/telemetry_collection_service.c
@@ -2,18 +2,47 @@
 
 #include <csp/csp.h>
 
-int collect_telemetry() {
+// Command Codes
+// values can be subject to change
+#define CMD_DUMMY 0x01
+#define CMD_GET_PARAM 0x02
 
-}
+// Command execution option
+#define ON_REAL_TIME 0x01
 
-int send_telemetry(uint8_t prio, uint8_t dest, subsystem_port_t port, uint32_t timeout, void * outbuf, int outlen, void * inbuf, int inlen) {
+/*
+  outbuf should be 2 chars larger than aux_buffer, aux_buffer should be 254 chars max for now
+  TODO: add check to make sure aux_butter isnt too large
+*/
+
+// same as sfsf get command
+int collect_telemetry(uint8_t prio, subsystem_address_t dest, subsystem_port_t port, uint32_t timeout, void * aux_buffer, int outlen, void * inbuf, int inlen) {
+    char outbuf[256];
+    snprintf(outbuf, sizeof(outbuf), "%c%c", CMD_GET_PARAM, ON_REAL_TIME);
     return csp_transaction(prio, dest, port, timeout, outbuf, outlen, inbuf, inlen);
 }
 
-int struct_to_char_buff(void * inbuf, uint32_t inlen) {
-
+// buffer doesn't need a command to send data, can just use dummy command
+int send_telemetry(uint8_t prio, subsystem_address_t dest, subsystem_port_t port, uint32_t timeout, void * aux_buffer, int outlen, void * inbuf, int inlen) {
+    char outbuf[256];
+    snprintf(outbuf, sizeof(outbuf), "%c%c", CMD_DUMMY, ON_REAL_TIME);
+    return csp_transaction(prio, dest, port, timeout, outbuf, outlen, inbuf, inlen);
 }
 
-int char_buff_to_struct(void * outbuf, uint32_t outlen) {
+int struct_to_char_buff(void * inbuf, uint32_t inlen, subsystem_parameters_t subsystem_param) {
+    // choose which helper to call
+    switch(subsystem_param) {
+        default:
+            break;
+    }
+    return 1;
+}
 
+int char_buff_to_struct(void * outbuf, uint32_t outlen, subsystem_parameters_t subsystem_param) {
+    // choose which helper to call
+    switch(subsystem_param) {
+        default:
+            break;
+    }
+    return 1;
 }
